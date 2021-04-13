6616d34bddc4da5b6f5b23db21fe59fe2b7ddf1c
nelsoneo <nelsoneo@gmail.com>
2021-04-13 00:35:15 -0300

    finalizando

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './src/components/common';
+import { Header, Spinner, Button, Card } from './src/components/common';
 import LoginForm from './src/components/LoginForm';
 
 
 class App extends Component {
+  state= { loggedIn: null };
+
   componentDidMount() {
     firebase.initializeApp({
         apiKey: 'AIzaSyCi8wsnyX_P7DTymbvEI7cx_TBYp1nUMvM',
@@ -16,14 +18,37 @@ class App extends Component {
         appId: '1:603734192973:web:59cb20378a880b8b0d7d04',
         measurementId: 'G-P5MLD5JP01'
     });
+
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true});
+      } else {
+        this.setState({ loggedIn: false});
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress= {() => firebase.auth().signOut()}>
+            Log Out
+          </Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner />;
+    }
   }
   
-
   render() {
     return (
       <View >
         <Header headText= 'Authentication' />
-          <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
@@ -36,6 +61,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  ButtonSytle: {
+    marginTop: -50
+  }
 });
 
 export default App;
